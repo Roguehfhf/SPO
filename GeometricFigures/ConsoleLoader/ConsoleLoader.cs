23c916a63434c39ecd9b664b934a177a0a142044
Roguehfhf <Andrei,rogue@gmail.com>
2017-06-06 06:36:15 +0700

    1-3 лаба

---
+++ GeometricFigures/ConsoleLoader/ConsoleLoader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using GeometricFigures;
+
+namespace ConsoleLoader
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var geometricsFigure = new List<INterfaceFigures>();
+            try
+            {
+                var rectangleFigure = new Rectangle(5, 7);
+                geometricsFigure.Add(rectangleFigure);
+
+                var circleFigure = new Circle(6.2);
+                geometricsFigure.Add(circleFigure);
+
+                var triangleFigure = new Triangle(4, 6, 9);
+                geometricsFigure.Add(triangleFigure);
+
+                var ringFigure = new Ring(9.8, 8);
+                geometricsFigure.Add(ringFigure);
+
+                foreach (INterfaceFigures figure in geometricsFigure)
+                {
+                    Console.WriteLine("{0}. Площадь = {1}. Периметр = {2}", figure.Type, figure.Area, figure.Perimeter);
+                }
+            }
+
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ GeometricFigures/GeometricsFigureView/Serialization.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using GeometricFigures;
+
+namespace GeometricsFigureView
+{
+    public static class Serialization
+    {
+        private static readonly BinaryFormatter _formatter = new BinaryFormatter();
+
+        public static void Serialize(string fileName, List<INterfaceFigures> file)
+        {
+            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                _formatter.Serialize(fs, file);
+            }
+        }
+
+        public static List<INterfaceFigures> Deserialize(string fileName)
+        {
+            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                var file = (List<INterfaceFigures>)_formatter.Deserialize(fs);
+                return file;
+            }
+        }
+    }
+}
+
