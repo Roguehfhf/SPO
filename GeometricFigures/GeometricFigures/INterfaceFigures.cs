48531deb91f8fceb4a316e1aefc1bad944c3a8ba
Roguehfhf <Andrei,rogue@gmail.com>
2017-06-12 17:46:38 +0700

    Чото добавил

---
+++ GeometricFigures/GeometricFigures/INterfaceFigures.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricFigures
+{
+    /// <summary>
+    /// Интерфейс геометрической фигуры.
+    /// </summary>
+    public interface INterfaceFigures
+    {
+        /// <summary>
+        /// Получение площади фигуры.
+        /// </summary>
+        /// <returns>Площадь фигуры.</returns>
+        double Area { get; }
+
+        /// <summary>
+        /// Получение периметра фигуры.
+        /// </summary>
+        /// <returns>Периметр фигуры.</returns>
+        double Perimeter { get; }
+
+        /// <summary>
+        /// Получение типа фигуры
+        /// </summary>
+        /// <returns>Тип фигуры</returns>
+        string Type { get; }
+    }
+}
