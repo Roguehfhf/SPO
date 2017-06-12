4025ccc1ed986443ccc329913e337b2f539d82e9
Roguehfhf <Andrei,rogue@gmail.com>
2017-06-12 17:49:58 +0700

    1-3я Лабы + часть 4ой

---
+++ GeometricFigures-2/GeometricFigures/GeometricFigures/Rectangle.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace GeometricFigures
+{
+    /// <summary>
+    /// Класс "Прямоугольник" 
+    /// </summary>
+    [Serializable]
+    public class Rectangle : IFigures
+    {
+        private double _height;
+        private double _width;
+
+        /// <summary>
+        ///  Инициализация высоты прямоугольника.
+        /// </summary>
+        public double Height
+        {
+            get { return _height; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Значение не может быть нулевым или меньше нуля");
+                }
+                _height = value;
+            }
+        }
+
+        /// <summary>
+        /// Инициализация ширины прямоугольника.
+        /// </summary>
+        public double Width
+        {
+            get { return _width; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Значение не может быть нулевым или меньше нуля");
+                }
+                _width = value;
+            }
+        }
+
+        /// <summary>
+        /// Пустой конструктор прямоугольника
+        /// </summary>
+        public Rectangle()
+        {
+        }
+
+        /// <summary>
+        /// конструктор прямоугольника
+        /// </summary>
+        public Rectangle(double height, double width)
+        {
+
+            Height = height;
+            Width = width;
+        }
+
+        /// <summary>
+        /// Площадь прямоугольника
+        /// </summary>
+        public double Area
+        {
+            get
+            {
+                if (_height <= 0 || _width <= 0)
+                {
+                    throw new ArgumentException("Прямоугольник не инициализирован значениями");
+                }
+                return (Height * Width);
+            }
+        }
+
+        /// <summary>
+        /// Периметр прямоугльника
+        /// </summary>
+        public double Perimeter
+        {
+            get
+            {
+                if (_height <= 0 || _width <= 0)
+                {
+                    throw new ArgumentException("Прямоугольник не инициализирован значениями");
+                }
+                return ((Height + Width) * 2);
+            }
+        }
+        /// <summary>
+        /// Получение типа фигуры
+        /// </summary>
+        /// <returns>Тип фигуры</returns>
+        public string Type
+        {
+            get
+            {
+                return "Прямоугольник";
+            }
+        }
+    }
+}
---
+++ GeometricFigures-2/GeometricFigures/GeometricsFigureView/Serialization.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using GeometricFigures;
+
+namespace GeometricsFigureView
+{
+    public static class Serialization
+    {
+        private static readonly BinaryFormatter _formatter = new BinaryFormatter();
+
+        public static void Serialize(string fileName, List<IFigures> file)
+        {
+            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                _formatter.Serialize(fs, file);
+
+            }
+        }
+
+        public static List<IFigures> Deserialize(string fileName)
+        {
+            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                var file = (List<IFigures>)_formatter.Deserialize(fs);
+                return file;
+            }
+        }
+    }
+}
+
