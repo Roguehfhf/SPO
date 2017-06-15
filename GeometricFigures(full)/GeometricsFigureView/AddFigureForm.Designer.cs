d4be0ff3b882b477b72bc6936c6a7e9130f7efc1
Roguehfhf <Andrei,rogue@gmail.com>
2017-06-16 04:57:22 +0700

    Форма для добавления фигуры

---
+++ GeometricFigures(full)/GeometricsFigureView/AddFigureForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace GeometricsFigureView
+{
+    partial class AddFigureForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OkButton = new System.Windows.Forms.Button();
+            this.CanselButton = new System.Windows.Forms.Button();
+            this.figureControl1 = new GeometricsFigureView.Controls.FigureControl();
+            this.SuspendLayout();
+            // 
+            // OkButton
+            // 
+            this.OkButton.Location = new System.Drawing.Point(124, 208);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(86, 25);
+            this.OkButton.TabIndex = 1;
+            this.OkButton.Text = "OK";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // CanselButton
+            // 
+            this.CanselButton.Location = new System.Drawing.Point(240, 208);
+            this.CanselButton.Name = "CanselButton";
+            this.CanselButton.Size = new System.Drawing.Size(86, 25);
+            this.CanselButton.TabIndex = 2;
+            this.CanselButton.Text = "Отмена";
+            this.CanselButton.UseVisualStyleBackColor = true;
+            this.CanselButton.Click += new System.EventHandler(this.CanselButton_Click);
+            // 
+            // figureControl1
+            // 
+            this.figureControl1.Figure = null;
+            this.figureControl1.Location = new System.Drawing.Point(12, 12);
+            this.figureControl1.Name = "figureControl1";
+            this.figureControl1.Size = new System.Drawing.Size(420, 190);
+            this.figureControl1.TabIndex = 0;
+            // 
+            // AddFigureForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 261);
+            this.Controls.Add(this.CanselButton);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.figureControl1);
+            this.Name = "AddFigureForm";
+            this.Text = "Создать фигуру";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controls.FigureControl figureControl1;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Button CanselButton;
+    }
+}
---
+++ GeometricFigures(full)/GeometricsFigureView/AddFigureForm.cs
@@ -0,0 +1,69 @@
+﻿using GeometricFigures;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeometricsFigureView
+{
+    public partial class AddFigureForm : Form
+    {
+        public AddFigureForm()
+        {
+            InitializeComponent();
+        }
+
+        public IFigures Figure
+        {
+            get
+            {
+                try
+                {
+                    var cathThrowFigure = figureControl1.Figure;
+                }
+                catch (FormatException exception)
+                {
+                    MessageBox.Show(exception.Message);
+                    return null;
+                }
+                return figureControl1.Figure;
+            }
+            set
+            {
+                try
+                {
+                    figureControl1.Figure = value;
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                    throw;
+                }
+            }
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            catch (Exception A)
+            {
+                MessageBox.Show(A.Message, @"Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void CanselButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
